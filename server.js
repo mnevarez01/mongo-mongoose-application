5f1a457f6c24658a55a4d9d8c5a1a291da35e06d
Maria Nevarez <mnevarez1220@gmail.com>
2020-05-14 17:38:28 -0600

    updated server.js

--- server.js
+++ server.js
@@ -15,20 +15,6 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-app.get("/")
-app.get("/stats")
-app.get("/exercise?", (req, res) => {
-  db.find({})
-    .then(dbWorkout => res.status(200).json(dbWorkout))
-    .catch(err => res.json(err))
-})
-
-app.post("/exercise?", ({ body }, res) => {
-  db.create(body)
-    .then(workout => res.json(workout))
-    .catch(err => res.status(500).json(err))
-});
-
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`)
