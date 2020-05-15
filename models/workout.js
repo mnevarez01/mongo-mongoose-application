d1fe0187f3a5ff67b44c856a01bc4a24b7fb46a9
Maria Nevarez <mnevarez1220@gmail.com>
2020-05-14 18:52:04 -0600

    added mongoose connection

--- models/workout.js
+++ models/workout.js
@@ -1,35 +1,60 @@
 const mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-  day: Date,
-  exercises: [
-    {
-      type: String,
-      name: String,
-      distance: Number,
-      weight: Number,
-      reps: Number,
-      sets: Number,
-      duration: Number
-    }
-  ]
-},
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: () => new Date()
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise name"
+        },
+        duration: {
+          type: Number,
+          required: "Enter an exercise duration in minutes"
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
+      }
+    ]
+  },
   {
     toJSON: {
-      virtual: true
+      // include any virtual properties when data is requested
+      virtuals: true
     }
-  })
-// WorkoutSchema.methods.totalDuration = function () {
-//   this.totalDuration = this.duration * this.reps
-//   return this.totalDuration
-// }
-WorkoutSchema.virtual("Total Duration").get(function () {
+  }
+);
+
+// adds a dynamically-created property to schema
+workoutSchema.virtual("totalDuration").get(function () {
+  // "reduce" array of exercises down to just the sum of their durations
   return this.exercises.reduce((total, exercise) => {
     return total + exercise.duration;
   }, 0);
-})
+});
 
-const Workout = mongoose.model("Workout", WorkoutSchema)
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI);
 
 app.use(require("./routes/api"));
 app.use(require("./routes/view"));
