062e0840432a00b604149c392c7307f4a3a5fdfb
Maria Nevarez <mnevarez1220@gmail.com>
2020-05-14 18:55:04 -0600

    updated connection

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+
 mongoose.connect(MONGODB_URI);
 
 app.use(require("./routes/api"));
